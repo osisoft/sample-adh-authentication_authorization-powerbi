2d584aede50b27ff098b1bbede77f7c7dc9d02e7
Doug Broad <dbroad@osisoft.com>
2021-03-16 09:55:51 -0400

    Add video recording

--- OCSConnectorTest/UnitTests.cs
+++ OCSConnectorTest/UnitTests.cs
@@ -4,6 +4,7 @@ using System.IO;
 using Newtonsoft.Json;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Interfaces;
 using OpenQA.Selenium.Appium.Windows;
 using Xunit;
 
@@ -34,110 +35,121 @@ namespace OCSConnectorTest
             var desktopOptions = new AppiumOptions();
             desktopOptions.AddAdditionalCapability("app", "Root");
             using var desktopSession = new WindowsDriver<WindowsElement>(appiumUri, desktopOptions);
+            desktopSession.StartRecordingScreen();
 
-            var powerBIWindow = desktopSession.TryFindElementByName("Untitled - Power BI Desktop");
-            var powerBIWindowHandle = powerBIWindow.GetAttribute("NativeWindowHandle");
-            powerBIWindowHandle = int.Parse(powerBIWindowHandle, CultureInfo.InvariantCulture)
-                .ToString("x", CultureInfo.InvariantCulture); // Convert to Hex
-
-            var powerBIOptions = new AppiumOptions();
-            powerBIOptions.AddAdditionalCapability("appTopLevelWindow", powerBIWindowHandle);
-            using var powerBISession = new WindowsDriver<WindowsElement>(appiumUri, powerBIOptions);
-
-            // Close Start window
-            var startWindow = powerBISession.TryFindElementByAccessibilityId("KoStartDialog");
-            if (startWindow != null)
+            try
             {
-                var closeStart = startWindow.FindElementByName("Close");
-                closeStart.Click();
-            }
+                var powerBIWindow = desktopSession.TryFindElementByName("Untitled - Power BI Desktop");
+                var powerBIWindowHandle = powerBIWindow.GetAttribute("NativeWindowHandle");
+                powerBIWindowHandle = int.Parse(powerBIWindowHandle, CultureInfo.InvariantCulture)
+                    .ToString("x", CultureInfo.InvariantCulture); // Convert to Hex
 
-            // Clear cached credentials
-            var queries = powerBISession.TryFindElementByName("Queries");
-            var transformData = queries.TryFindElementsByName("Transform data");
-            var dataSourceSettings = powerBISession.TryClickAndFindElementByName(transformData[2], "Data source settings");
-            var dataSourceSettingsDialog = powerBISession.TryClickAndFindElementByAccessibilityId(dataSourceSettings, "ManageDataSourcesDialog");
-            var clearPermissions = dataSourceSettingsDialog.TryFindElementsByName("Clear Permissions");
-            var clearPermissionsDialog = dataSourceSettingsDialog.TryClickAndFindElementByAccessibilityId(clearPermissions[1], "MessageDialog", 10);
-            if (clearPermissionsDialog != null)
+                var powerBIOptions = new AppiumOptions();
+                powerBIOptions.AddAdditionalCapability("appTopLevelWindow", powerBIWindowHandle);
+                using var powerBISession = new WindowsDriver<WindowsElement>(appiumUri, powerBIOptions);
+
+                // Close Start window
+                var startWindow = powerBISession.TryFindElementByAccessibilityId("KoStartDialog");
+                if (startWindow != null)
+                {
+                    var closeStart = startWindow.FindElementByName("Close");
+                    closeStart.Click();
+                }
+
+                // Clear cached credentials
+                var queries = powerBISession.TryFindElementByName("Queries");
+                var transformData = queries.TryFindElementsByName("Transform data");
+                var dataSourceSettings = powerBISession.TryClickAndFindElementByName(transformData[2], "Data source settings");
+                var dataSourceSettingsDialog = powerBISession.TryClickAndFindElementByAccessibilityId(dataSourceSettings, "ManageDataSourcesDialog");
+                var clearPermissions = dataSourceSettingsDialog.TryFindElementsByName("Clear Permissions");
+                var clearPermissionsDialog = dataSourceSettingsDialog.TryClickAndFindElementByAccessibilityId(clearPermissions[1], "MessageDialog", 10);
+                if (clearPermissionsDialog != null)
+                {
+                    var delete = clearPermissionsDialog.TryFindElementsByName("Delete");
+                    delete[1].Click();
+                }
+
+                var close = dataSourceSettingsDialog.TryFindElementByName("Close");
+                close.Click();
+
+                // Open OCS Connector
+                var getData = powerBISession.TryFindElementByName("Get data");
+                var getDataWindow = powerBISession.TryClickAndFindElementByAccessibilityId(getData, "DataSourceGalleryDialog");
+                var search = getDataWindow.FindElementByName("Search");
+                search.SendKeys("OSI");
+
+                var sample = getDataWindow.TryFindElementByName("OSIsoft Cloud Services Sample (Beta)");
+                var connect = getDataWindow.TryClickAndFindElementByName(sample, "Connect");
+
+                // Enter query info
+                var builderDialog = powerBISession.TryClickAndFindElementByAccessibilityId(connect, "BuilderDialog");
+                var uri = builderDialog.TryFindElementsByName("OSIsoft Cloud Services API Path");
+                uri[1].SendKeys($"{Settings.OcsAddress}/api/v1/Tenants/{Settings.OcsTenantId}/Namespaces");
+
+                var timeout = builderDialog.TryFindElementsByName("Timeout (optional)");
+                timeout[1].SendKeys("100");
+
+                var ok = builderDialog.TryFindElementByName("OK");
+                var signin = builderDialog.TryClickAndFindElementByName(ok, "Sign in");
+
+                // Sign in
+                var oauthDialog = powerBISession.TryClickAndFindElementByAccessibilityId(signin, "OAuthDialog");
+                var personalAccount = oauthDialog.TryFindElementsByName("Personal Account");
+                Assert.NotNull(personalAccount);
+                personalAccount[1].Click();
+                var email = oauthDialog.TryFindElementByAccessibilityId("i0116", 10);
+                if (email == null)
+                {
+                    // Try going back and choosing "Use another account"
+                    var back = oauthDialog.TryFindElementByAccessibilityId("idBtn_Back");
+                    back.Click();
+                    var otherAccount = oauthDialog.TryFindElementByName("Use another account");
+                    otherAccount.Click();
+                    email = oauthDialog.TryFindElementByAccessibilityId("i0116");
+                }
+
+                email.SendKeys(Settings.Login);
+
+                var next = oauthDialog.TryFindElementByAccessibilityId("idSIButton9");
+                next.Click();
+                var pwd = oauthDialog.TryFindElementByAccessibilityId("i0118");
+                if (pwd != null)
+                {
+                    // This step can be skipped if account is saved
+                    pwd.SendKeys(Settings.Password);
+                    pwd.SendKeys(Keys.Enter);
+                    builderDialog = powerBISession.TryClickAndFindElementByAccessibilityId(connect, "BuilderDialog");
+                }
+                else
+                {
+                    builderDialog = powerBISession.TryFindElementByAccessibilityId("BuilderDialog");
+                }
+
+                connect = builderDialog.TryFindElementByName("Connect");
+                connect.Click();
+
+                // Find Power Query Editor window
+                var queryEditorWindow = desktopSession.TryFindElementByName("Untitled - Power Query Editor");
+                var queryEditorWindowHandle = queryEditorWindow.GetAttribute("NativeWindowHandle");
+                queryEditorWindowHandle = int.Parse(queryEditorWindowHandle, CultureInfo.InvariantCulture)
+                    .ToString("x", CultureInfo.InvariantCulture); // Convert to Hex
+
+                var queryEditorOptions = new AppiumOptions();
+                queryEditorOptions.AddAdditionalCapability("appTopLevelWindow", queryEditorWindowHandle);
+                using var queryEditorSession = new WindowsDriver<WindowsElement>(appiumUri, queryEditorOptions);
+
+                // Verify results
+                var record = queryEditorSession.TryFindElementByName("Record");
+                var self = queryEditorSession.TryClickAndFindElementByName(record, "Self");
+                Assert.NotNull(self);
+            }
+            catch
             {
-                var delete = clearPermissionsDialog.TryFindElementsByName("Delete");
-                delete[1].Click();
+                var video = desktopSession.StopRecordingScreen();
+                var decoded = Convert.FromBase64String(video);
+                File.WriteAllBytes("test_recording.mp4", decoded);
+                throw;
             }
-
-            var close = dataSourceSettingsDialog.TryFindElementByName("Close");
-            close.Click();
-
-            // Open OCS Connector
-            var getData = powerBISession.TryFindElementByName("Get data");
-            var getDataWindow = powerBISession.TryClickAndFindElementByAccessibilityId(getData, "DataSourceGalleryDialog");
-            var search = getDataWindow.FindElementByName("Search");
-            search.SendKeys("OSI");
-
-            var sample = getDataWindow.TryFindElementByName("OSIsoft Cloud Services Sample (Beta)");
-            var connect = getDataWindow.TryClickAndFindElementByName(sample, "Connect");
-
-            // Enter query info
-            var builderDialog = powerBISession.TryClickAndFindElementByAccessibilityId(connect, "BuilderDialog");
-            var uri = builderDialog.TryFindElementsByName("OSIsoft Cloud Services API Path");
-            uri[1].SendKeys($"{Settings.OcsAddress}/api/v1/Tenants/{Settings.OcsTenantId}/Namespaces");
-
-            var timeout = builderDialog.TryFindElementsByName("Timeout (optional)");
-            timeout[1].SendKeys("100");
-
-            var ok = builderDialog.TryFindElementByName("OK");
-            var signin = builderDialog.TryClickAndFindElementByName(ok, "Sign in");
-
-            // Sign in
-            var oauthDialog = powerBISession.TryClickAndFindElementByAccessibilityId(signin, "OAuthDialog");
-            var personalAccount = oauthDialog.TryFindElementsByName("Personal Account");
-            Assert.NotNull(personalAccount);
-            personalAccount[1].Click();
-            var email = oauthDialog.TryFindElementByAccessibilityId("i0116", 10);
-            if (email == null)
-            {
-                // Try going back and choosing "Use another account"
-                var back = oauthDialog.TryFindElementByAccessibilityId("idBtn_Back");
-                back.Click();
-                var otherAccount = oauthDialog.TryFindElementByName("Use another account");
-                otherAccount.Click();
-                email = oauthDialog.TryFindElementByAccessibilityId("i0116");
-            }
-
-            email.SendKeys(Settings.Login);
-
-            var next = oauthDialog.TryFindElementByAccessibilityId("idSIButton9");
-            next.Click();
-            var pwd = oauthDialog.TryFindElementByAccessibilityId("i0118");
-            if (pwd != null)
-            {
-                // This step can be skipped if account is saved
-                pwd.SendKeys(Settings.Password);
-                pwd.SendKeys(Keys.Enter);
-                builderDialog = powerBISession.TryClickAndFindElementByAccessibilityId(connect, "BuilderDialog");
-            }
-            else
-            {
-                builderDialog = powerBISession.TryFindElementByAccessibilityId("BuilderDialog");
-            }
-
-            connect = builderDialog.TryFindElementByName("Connect");
-            connect.Click();
-
-            // Find Power Query Editor window
-            var queryEditorWindow = desktopSession.TryFindElementByName("Untitled - Power Query Editor");
-            var queryEditorWindowHandle = queryEditorWindow.GetAttribute("NativeWindowHandle");
-            queryEditorWindowHandle = int.Parse(queryEditorWindowHandle, CultureInfo.InvariantCulture)
-                .ToString("x", CultureInfo.InvariantCulture); // Convert to Hex
-
-            var queryEditorOptions = new AppiumOptions();
-            queryEditorOptions.AddAdditionalCapability("appTopLevelWindow", queryEditorWindowHandle);
-            using var queryEditorSession = new WindowsDriver<WindowsElement>(appiumUri, queryEditorOptions);
-
-            // Verify results
-            var record = queryEditorSession.TryFindElementByName("Record");
-            var self = queryEditorSession.TryClickAndFindElementByName(record, "Self");
-            Assert.NotNull(self);
         }
     }
 }
